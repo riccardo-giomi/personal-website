9059583805bcf203452781539af207a989b0547c
Riccardo Giomi <contact@riccardogiomi.com>
2021-02-08 11:05:01 +0100

    Changed icons.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    iconPaths: {
+      favicon32: 'img/icons/terminal-icon-16x16.png',
+      favicon16: 'img/icons/terminal-icon-32x32.png'
+    }
+  }
 }
